bfe260fbd2ea877d60834b0fde66c4482fe13843
Oshiro Kazuki <oshirokazuki@aim.com>
2026-04-04 12:18:06 +0900

    chore(deploy): add open-next.config.ts for non-interactive build

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // OpenNext Cloudflare Adapter Configuration
+  // Details: https://opennext.js.org/cloudflare
+});
